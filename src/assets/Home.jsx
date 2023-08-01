fa0b4831244b3ffbb29ec3d600816b3095efd1a3
Caleb D. Sim <dylansim168@gmail.com>
2023-08-01 17:04:35 +0800

    minor responsive fix

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -4,7 +4,7 @@ export default function Home(){
 
     return(
     <div className="Asset-content home-main-div min-h-full p-4">
-         <div className="border-black p-6 md:max-w-xl">
+         <div className="border-black p-6  w-full max-w-xs md:max-w-xl">
           <h1 className="text-5xl text-black font-bold">CALEB D. SIM</h1>
           <p className="text-2xl pt-2">Junior Front End Developer utilizing <span>CSS</span>, <span>JS</span>, <span>REACT</span>, and <span>TAILWIND</span></p>
         </div>
