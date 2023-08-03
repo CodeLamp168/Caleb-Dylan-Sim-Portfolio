6cdd0f0824af0b216731ba7d0576c6187bf10c94
Caleb D. Sim <dylansim168@gmail.com>
2023-08-03 12:27:21 +0800

    Home jsx additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,56 @@
 import Home from "./assets/Home"
 
 function App() {
+ function LoadingScreen(){
+
+
+    return(
+      <div className="page-loading fixed w-screen min-h-screen z-50">
+
+        <div className="w-screen min-h-screen relative grid place-content-center">
+
+          <div className="loading-text border-white border-r border-l rounded  text-center overflow-hidden py-4">
+            <h1 className="uppercase text-white text-3xl w-max break-words px-4">Caleb Sim <span className="text-5xl">Portfolio</span></h1>
+          </div>
+
+        </div>
+
+      </div>
+     
+    )
+  }
 
   return (
-    <div className="App flex flex-col md:flex-row justify-items-center">
+    <div className="App flex flex-col md:flex-row justify-items-center relative md:mx-10 lg:mx-16 ">
 
-       <nav className="main-nav font-semibold mx-4  md:mx-0 md:min-h-screen md:max-w-min md:py-8 md:pt-24 relative md:ml-6 ">
-        <div className="nav-line main-nav-border-line transition-transform"></div>
-          <div className="nav-list flex gap-4 py-4 md:py-0 md:gap-0 md:flex-col items-center relative">
-            <div className="nav-item max-w-max md:mb-10 md:-rotate-90 transition-transform hover:scale-110">
+      
+       <nav className="main-nav  border-black border-b md:border-none font-semibold mx-4  md:mx-0 md:min-h-screen md:max-w-min md:py-8 md:pt-24 relative">
+        <div className="nav-line main-nav-border-line hidden md:block "></div>
+          <div className="nav-list flex md:space-y-16 gap-2 py-4 md:py-0 md:gap-0 md:flex-col items-center relative ">
+            <div className="nav-item max-w-max  md:-rotate-90 transition-transform hover:scale-110">
               <a href="#">Home</a>
             </div>
-            <div className="nav-item max-w-max md:mb-12 md:-rotate-90 transition-transform hover:scale-110">
-              <a href="#">About</a>
-            </div>
-            <div className="nav-item max-w-max md:mb-12 md:-rotate-90 transition-transform hover:scale-110">
+            <div className="nav-item max-w-max  md:-rotate-90 transition-transform hover:scale-110">
               <a href="#">Projects</a>
             </div>
-            <div className="nav-item max-w-max md:mt-2 md:-rotate-90 transition-transform hover:scale-110">
+            <div className="nav-item max-w-max  md:-rotate-90 transition-transform hover:scale-110">
               <a href="#">Contacts</a>
             </div>
-            <div className="nav-item max-w-max md:mt-10 md:-rotate-90 transition-transform hover:scale-110">
+            <div className="nav-item max-w-max  md:-rotate-90 transition-transform hover:scale-110">
               <a href="#">CVC</a>
             </div>
             <div className="nav-line line-inside-nav transition-transform"></div>
           </div>
-          <div className="nav-line main-nav-border-line transition-transform"></div>
+          <div className="nav-line main-nav-border-line transition-transform hidden md:block "></div>
 
         </nav>
 
-      <div className="main-wrap  border-black  md:border-r md:p-0 md:p-4 md:my-6 md:mx-4 overflow-y-scroll">
+      <div className="main-wrap  border-black   md:border-r md:p-0 md:p-4 md:my-6 md:mr-6 overflow-y-scroll">
         <Home/>
       </div>
-   
+
+
+  
     </div>
   )
 }
--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -3,10 +3,13 @@ export default function Home(){
 
 
     return(
-    <div className="Asset-content home-main-div min-h-full p-4">
-         <div className="border-black p-6  w-full max-w-xs md:max-w-xl">
-          <h1 className="text-5xl text-black font-bold">CALEB D. SIM</h1>
-          <p className="text-2xl pt-2">Junior Front End Developer utilizing <span>CSS</span>, <span>JS</span>, <span>REACT</span>, and <span>TAILWIND</span></p>
+    <div className="Asset-content home-main-div flex flex-col  items-center md:block min-h-full p-4  relative">
+         <div className="border-black py-6 text-center md:text-left w-full max-w-xs md:max-w-xl">
+          <h1 className="text-5xl text-black font-semibold">CALEB SIM</h1>
+          <p className="text-2xl pt-2 font-medium">Front End Developer</p>
+        </div>
+        <div className="md:absolute  md:text-2xl w-44 md:right-14 md:bottom-1">
+            <p className="font-medium text-center md:text-left">A junior front end developer. I strive to make the best out of my abilities. Creating websites and web assets with <span className="font-semibold text-3xl">JS, CSS, SCSS, REACT, & TAILWIND</span></p>
         </div>
     </div>
     )
