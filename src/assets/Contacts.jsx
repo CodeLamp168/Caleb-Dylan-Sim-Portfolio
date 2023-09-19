23a0de6efd8e843100bb76bc5fa84757bd6e0de5
Caleb D. Sim <dylansim168@gmail.com>
2023-09-19 09:21:17 +0800

    Contact Form

--- src/assets/Contacts.jsx
+++ src/assets/Contacts.jsx
@@ -1,12 +1,40 @@
-export default function Contacts(){
+import PageHeader from "./small-components/PageHeader";
+import Discord from "/svgs/discord.svg";
+import Instagram from "/svgs/instagram.svg";
+import LinkedIn from "/svgs/linkedin.svg";
+import Github from "/svgs/github.svg";
+
+function ContactList() {
+
+    const ImageLink = ({ href = '#', imgSrc }) => {
 
 
+        return (
+          <a href={href}>
+            <img
+              src={imgSrc}
+              alt="image"
+              className="w-8 h-8 transform scale-100 hover:scale-110 transition-transform duration-300"
+            />
+          </a>
+        );
+      };
 
-    return(
-    <div className="Asset-content min-h-full p-4">
-         <div className="border border-red-200 md:max-w-xl">
-          <h1 className="text-5xl">Contacts</h1>
-        </div>
+  return (
+    <div class="contact-list-wrap">
+      <ul className="flex flex-row w-64 gap-8 contact-list-ul-container">
+        <ImageLink href={'https://github.com/CodeLamp168'} imgSrc={Github} />
+        <ImageLink href={'https://www.linkedin.com/in/calebsimdev/'} imgSrc={LinkedIn} />
+      </ul>
     </div>
-    )
+  );
+}
+
+export default function Contacts() {
+  return (
+    <div className="Asset-content min-h-full p-2">
+      <PageHeader pageTitle={`Contacts`} />
+      <ContactList />
+    </div>
+  );
 }
--- src/assets/Projects.jsx
+++ src/assets/Projects.jsx
@@ -1,17 +1,7 @@
 import React, { useState } from 'react';
+import PageHeader from './small-components/PageHeader';
 
 export default function Projects(){
-
-
-
-    function ProjectHeader() {
-
-        return (
-          <div className='title-header py-6 text-center md:text-left w-full max-w-xs md:max-w-xl z-10'>
-            <h1 className="text-7xl lg:text-8xl text-black font-semibold">Projects</h1>
-          </div>
-        );
-    }
 
     function ProjectLists(){
 
@@ -53,7 +43,7 @@ export default function Projects(){
         }
         function FrontEndMentor() {
             return (
-              <div className="flex flex-col items-center md:items-end front-end-mentor-challenge-list">
+              <div className="flex flex-col items-center md:items-end front-end-mentor-challenge-list border-red-500">
   
                 <div className="flex flex-col items-end gap-12 md:gap-12">
                   <ProjectItem Tools={<ReactSCSS />} ProjectName={`Space Program Tourism Site`} weblink={`https://codelamp168.github.io/Space-Tourism-Site/`} />
@@ -61,7 +51,7 @@ export default function Projects(){
                   <ProjectItem Tools={<ReactCSS />} ProjectName={`Room HomePage`} weblink={`https://codelamp168.github.io/room-homepage-vite-react/`} />
                   <ProjectItem Tools={<VanillaJS />} ProjectName={`News Homepage`} weblink={`https://codelamp168.github.io/News-Homepage-Front-End-Mentor-CHALLENGE/`} />
                   <ProjectItem Tools={<VanillaJS />} ProjectName={`Interactive Card Details`} weblink={`https://codelamp168.github.io/Interactive-Card-Details/`} />
-                  <ProjectItem Tools={<VanillaHTML />} ProjectName={`Crowd Funding Page`} weblink={`https://codelamp168.github.io/FED-Challenge-SCSS-and-JS-Crowdfunding-Page/`} />
+              
                 </div>
                 <div className="invisible-div-for-fade-effect w-full h-2 md:h-4 mt-24"></div>
               </div>
@@ -94,7 +84,7 @@ export default function Projects(){
 
     return(
     <div className="Asset-content home-main-div flex flex-col  items-center md:block min-h-full p-4  relative z-0 ">
-        <ProjectHeader/>
+        <PageHeader pageTitle={`Project`}/>
         <ProjectLists />
     </div>
     )
